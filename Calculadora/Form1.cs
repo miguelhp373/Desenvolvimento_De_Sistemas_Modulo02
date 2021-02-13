6506c3c85d4dba130a1f4546301313de6945cdec
Miguel Henrique <miguelhp373@gmail.com>
2021-02-12 22:13:27 -0300

    Aula 01 Dia 12-02

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        decimal num1, num2, resp;
+        string Operacao;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            //if(comboBox1.SelectedItem.ToString() == "Adição"){
+              //  label1.Text = "Teste";
+            //}
+            
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)//subtracao
+        {
+            num1 = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = null;
+            button13.Enabled = true;
+            Operacao = "Sub";
+        }
+
+        private void button16_Click(object sender, EventArgs e)//multiplicação
+        {
+            num1 = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = null;
+            button13.Enabled = true;
+            Operacao = "Mult";
+        }
+
+        private void button15_Click(object sender, EventArgs e)//divisao
+        {
+            num1 = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = null;
+            button13.Enabled = true;
+            Operacao = "Div";
+        }
+
+        private void button14_Click(object sender, EventArgs e)//limpar
+        {
+            num1 = 0;
+            num2 = 0;
+            textBox1.Text = null;
+            Operacao = "";
+            button13.Enabled = false;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ".";
+        }
+
+        private void button13_Click(object sender, EventArgs e)//igual
+        {
+            num2 = Convert.ToDecimal(textBox1.Text);
+            
+
+            if(Operacao == "Som")
+            {
+                resp = num1 + num2;
+            }else if (Operacao == "Sub")
+            {
+                resp = num1 - num2;
+            }else if (Operacao == "Mult")
+            {
+                resp = num1 * num2;
+            }else if (Operacao == "Div")
+            {
+                resp = num1 / num2;
+            }
+            
+            textBox1.Text = resp.ToString();
+            button13.Enabled = false;
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)//soma
+        {
+            //button12.Enabled = false;
+            num1 = Convert.ToDecimal(textBox1.Text);
+            textBox1.Text = null;
+            button13.Enabled = true;
+            Operacao = "Som";
+        }
+    }
+}
---
+++ Primeiro Projeto/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Primeiro_Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira Algum Texto";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(281, 102);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
